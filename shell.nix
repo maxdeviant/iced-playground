641b3d1c4a270ca058f131e90cbd2be8f9de9d1b
Marshall Bowers <elliott.codes@gmail.com>
2019-11-26 21:37:40 -0500

    Load Vulkan
    Load Vulkan

--- shell.nix
+++ shell.nix
@@ -16,5 +16,5 @@ stdenv.mkDerivation {
     xorg.libXrandr
   ];
 
-  LD_LIBRARY_PATH = "${lib.makeLibraryPath [ x11 xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr ]}";
+  LD_LIBRARY_PATH = "${lib.makeLibraryPath [ vulkan-loader x11 xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr ]}";
 }
